9871c9c200550617e9abd573776b06424d616c0d
Eckl, Máté <ecklm94@gmail.com>
2017-03-29 10:49:30 +0200

    Fix a minor typo

--- conf_example.py
+++ conf_example.py
@@ -1,6 +1,6 @@
 # This is an example config file which holds placeholders for the variables needed
 # to connect to the LorIoT websocket service.
-# To make it active copy to a file named conf.py. For security reasons conf.py
+# To make it active, copy to a file named conf.py. For security reasons conf.py
 # is ignored because it stores confidential data.
 
 # LorIoT parameters
